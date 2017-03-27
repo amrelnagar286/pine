d6f3addf9280e14598317c8a079167390aaabe21
Chen Xin Nien <chen.xin.nien@gmail.com>
2017-03-27 16:41:49 +0800

    update

---
+++ pine-base/src/com/netsteadfast/pine/util/PubHandlerCallable.java
@@ -0,0 +1,109 @@
+/* 
+ * Copyright 2012-2017 netsteadfast of copyright Chen Xin Nien
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * -----------------------------------------------------------------------
+ * 
+ * author: 	Chen Xin Nien
+ * contact: chen.xin.nien@gmail.com
+ * 
+ */
+package com.netsteadfast.pine.util;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.netsteadfast.base.SysMessageUtil;
+import com.netsteadfast.base.SysMsgConstants;
+import com.netsteadfast.base.model.ScriptTypeCode;
+import com.netsteadfast.base.model.YesNo;
+import com.netsteadfast.pine.client.ClientData;
+import com.netsteadfast.pine.client.ClientUtils;
+import com.netsteadfast.util.ScriptExpressionUtils;
+import com.netsteadfast.vo.PublishVO;
+
+public class PubHandlerCallable implements Callable<PublishVO> {
+	private PublishVO data;
+	private boolean run = true;
+	
+	public PubHandlerCallable(PublishVO publish) {
+		this.data = publish;
+	}
+	
+	public PublishVO getData() {
+		return data;
+	}
+
+	public void setData(PublishVO data) {
+		this.data = data;
+	}
+
+	public boolean isRun() {
+		return run;
+	}
+
+	public void setRun(boolean run) {
+		this.run = run;
+	}
+
+	private void action() throws Exception {
+		if (null == this.data) {
+			throw new Exception(SysMessageUtil.get(SysMsgConstants.PARAMS_INCORRECT));
+		}
+		long intervalSecMs = Integer.parseInt( this.data.getIntervalSec() ) * 1000;
+		String content = "";
+		if (!StringUtils.isBlank(this.data.getContent())) {
+			content = this.data.getContent();
+		}
+		if (StringUtils.isBlank(content) && !StringUtils.isBlank(this.data.getContentExpr())) {
+			Map<String, Object> paramMap = new HashMap<String, Object>();
+			paramMap.put("publish", this.data);
+			paramMap.put("value", "");
+			ScriptExpressionUtils.execute(ScriptTypeCode.GROOVY, this.data.getContentExpr(), paramMap, paramMap);
+			content = StringUtils.defaultString( (String)paramMap.get("value") );
+		}
+		ClientData client = ClientUtils.get(this.data.getClientId());
+		if (null == client) {
+			ClientUtils.add(this.data.getClientId(), this.data.getBkBrokerAddr(), Integer.parseInt(this.data.getQos()), this.data.getBkUsername(), this.data.getBkPassword(), this.data.getTopic(), "");
+			client = ClientUtils.get(this.data.getClientId());
+		}
+		
+		if ( intervalSecMs < 60000 ) { // 小於 60 秒的, FIRST_ON_START 一定是 'Y'
+			ClientUtils.publish(this.data.getClientId(), this.data.getTopic(), this.data.getEventId(), this.data.getName(), this.data.getScriptId(), this.data.getScriptType(), content, "");
+			ClientUtils.disconnect(this.data.getClientId());
+			this.run = false;
+			return;
+		}
+		
+		if (YesNo.YES.equals(this.data.getFirstOnStart())) {
+			ClientUtils.publish(this.data.getClientId(), this.data.getTopic(), this.data.getEventId(), this.data.getName(), this.data.getScriptId(), this.data.getScriptType(), content, "");
+		}
+		
+		while (this.run) {
+			Thread.sleep(intervalSecMs);
+			ClientUtils.publish(this.data.getClientId(), this.data.getTopic(), this.data.getEventId(), this.data.getName(), this.data.getScriptId(), this.data.getScriptType(), content, "");
+		}
+		
+	}
+
+	@Override
+	public PublishVO call() throws Exception {
+		this.action();
+		return this.data;
+	}	
+	
+}
---
+++ pine-base/src/com/netsteadfast/pine/util/PublishUtils.java
@@ -0,0 +1,159 @@
+/* 
+ * Copyright 2012-2017 netsteadfast of copyright Chen Xin Nien
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * -----------------------------------------------------------------------
+ * 
+ * author: 	Chen Xin Nien
+ * contact: chen.xin.nien@gmail.com
+ * 
+ */
+package com.netsteadfast.pine.util;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+
+import com.netsteadfast.base.AppContext;
+import com.netsteadfast.base.SysMessageUtil;
+import com.netsteadfast.base.SysMsgConstants;
+import com.netsteadfast.base.exception.ServiceException;
+import com.netsteadfast.base.model.DefaultResult;
+import com.netsteadfast.pine.client.ClientUtils;
+import com.netsteadfast.pine.service.IPublishService;
+import com.netsteadfast.po.PiPublish;
+import com.netsteadfast.vo.PublishVO;
+
+public class PublishUtils {
+	protected static Logger logger = Logger.getLogger(PublishUtils.class);
+	private static Map<String, PubHandlerCallable> pubHandlerMap = new HashMap<String, PubHandlerCallable>();
+	
+	@SuppressWarnings("unchecked")
+	public static PublishVO getPublish(String oid) throws ServiceException, Exception {
+		if (StringUtils.isBlank(oid)) {
+			throw new ServiceException(SysMessageUtil.get(SysMsgConstants.PARAMS_BLANK));
+		}		
+		IPublishService<PublishVO, PiPublish, String> publishService = (IPublishService<PublishVO, PiPublish, String>)
+				AppContext.getBean("pine.service.PublishService");
+		PublishVO publish = new PublishVO();
+		publish.setOid(oid);
+		DefaultResult<PublishVO> result = publishService.findObjectByOid(publish);
+		if ( result.getValue() == null ) {
+			throw new ServiceException( result.getSystemMessage().getValue() );
+		}
+		publish = result.getValue();
+		return publish;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static PublishVO getPublishById(String clientId) throws ServiceException, Exception {
+		if (StringUtils.isBlank(clientId)) {
+			throw new ServiceException(SysMessageUtil.get(SysMsgConstants.PARAMS_BLANK));
+		}		
+		IPublishService<PublishVO, PiPublish, String> publishService = (IPublishService<PublishVO, PiPublish, String>)
+				AppContext.getBean("pine.service.PublishService");
+		PublishVO publish = new PublishVO();
+		publish.setClientId(clientId);
+		DefaultResult<PublishVO> result = publishService.findByUK(publish);
+		if ( result.getValue() == null ) {
+			throw new ServiceException( result.getSystemMessage().getValue() );
+		}
+		publish = result.getValue();
+		return publish;
+	}	
+	
+	public static boolean isFound(String id) {
+		if (ClientUtils.get(id) == null) {
+			return false;
+		}
+		return true;
+	}	
+	
+	public static boolean isRun(String id) {
+		if ( pubHandlerMap.get(id) != null && pubHandlerMap.get(id).isRun() ) {
+			return true;
+		}
+		return false;
+	}
+	
+	public void pub(String oid) throws ServiceException, Exception {
+		PublishVO publish = PublishUtils.getPublish(oid);
+		pub( publish );
+	}	
+	
+	public void pubById(String clientId) throws ServiceException, Exception {
+		PublishVO publish = PublishUtils.getPublishById(clientId);
+		pub( publish );		
+	}	
+	
+	public void pub(PublishVO publish) throws ServiceException, Exception {
+		if (null == publish) {
+			throw new ServiceException(SysMessageUtil.get(SysMsgConstants.PARAMS_INCORRECT));
+		}
+		clear( publish );
+		ExecutorService executorService = Executors.newFixedThreadPool( 1 );
+		PubHandlerCallable pubHandler = null;
+		executorService.submit( (pubHandler = new PubHandlerCallable(publish)) );
+		pubHandlerMap.put(publish.getClientId(), pubHandler);
+	}
+	
+	public void stop(String oid) throws ServiceException, Exception {
+		PublishVO publish = PublishUtils.getPublish(oid);
+		stop( publish );
+	}	
+	
+	public void stopById(String clientId) throws ServiceException, Exception {
+		PublishVO publish = PublishUtils.getPublishById(clientId);
+		stop( publish );		
+	}	
+	
+	public void stop(PublishVO publish) throws ServiceException, Exception {
+		if (null == publish) {
+			throw new ServiceException(SysMessageUtil.get(SysMsgConstants.PARAMS_INCORRECT));
+		}
+		if ( pubHandlerMap.get(publish.getClientId()) == null ) {
+			return;
+		}
+		PubHandlerCallable pubHandler = pubHandlerMap.get(publish.getClientId());
+		pubHandler.setRun(false);
+		ClientUtils.disconnect( publish.getClientId() );
+	}
+	
+	public void clear(String oid) throws ServiceException, Exception {
+		PublishVO publish = PublishUtils.getPublish(oid);
+		clear( publish );
+	}	
+	
+	public void clearById(String clientId) throws ServiceException, Exception {
+		PublishVO publish = PublishUtils.getPublishById(clientId);
+		clear( publish );		
+	}	
+	
+	public void clear(PublishVO publish) throws ServiceException, Exception {
+		if (null == publish) {
+			throw new ServiceException(SysMessageUtil.get(SysMsgConstants.PARAMS_INCORRECT));
+		}
+		if ( pubHandlerMap.get(publish.getClientId()) == null ) {
+			return;
+		}
+		stop( publish );
+		pubHandlerMap.remove( publish.getClientId() );
+		ClientUtils.remove( publish.getClientId() );
+	}
+	
+}
